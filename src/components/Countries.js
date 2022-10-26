1756f33a10a2a2676ebd5ed2490bf09c586effd0
ellisvelandia <eyis619@gmail.com>
2022-10-26 17:47:04 -0500

    selectors

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -3,20 +3,79 @@ import Article from "./Article";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const regions = [
+    {
+      name: "Europe",
+    },
+    {
+      name: "Asia",
+    },
+    {
+      name: "Africa",
+    },
+    {
+      name: "Oceania",
+    },
+    {
+      name: "Americas",
+    },
+    {
+      name: "Antarctic",
+    },
+  ];
+
+  useEffect(() => {
+    document.title = `Showing All Countries`;
+  }, []);
 
   useEffect(() => {
     const getCountries = async () => {
       try {
         const res = await fetch("https://restcountries.com/v3.1/all");
         const data = await res.json();
-        setCountries(data.slice(0,10));
-        console.log(data)
+        setCountries(data.slice(0, 10));
+        console.log(data);
       } catch (error) {
         console.log(error);
       }
     };
     getCountries();
   }, []);
+
+  async function searchCountry() {
+    try {
+      const res = await fetch(
+        `https://restcountries.com/v3.1/name/${searchText}`
+      );
+      const data = await res.json();
+      setCountries(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async function filterByRegion(region) {
+    try {
+      const res = await fetch(
+        `https://restcountries.com/v3.1/region/${region}`
+      );
+      const data = await res.json();
+      setCountries(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function handleSearchCountry(e) {
+    e.preventDefault();
+    searchCountry();
+  }
+
+  function handleFilterByRegion(e) {
+    e.preventDefault();
+    filterByRegion();
+  }
 
   return (
     <>
@@ -26,11 +85,45 @@ const Countries = () => {
         </h1>
       ) : (
         <section className="container mx-auto p-8">
-        <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
-        {countries.map((country) => (
-          <Article key={country.name.common} {...country}/>
-         ))}
-        </div>
+          <div className="flex flex-col gap-4 md:flex-row md:items-center md:justify-between mb-8">
+            <form
+              onSubmit={handleSearchCountry}
+              autoComplete="off"
+              classNamr="max-w-4xl md:flex-1"
+            >
+              <input
+                type="text"
+                name="search"
+                id="search"
+                placeholder="Search for a country by its name"
+                required
+                value={searchText}
+                onChange={(e) => setSearchText(e.target.value)}
+                className="w-80 py-3 px-4 text-gray-600 placeholder-gray-600  shadow rounded outline-none dark:text-gray-400 dark:placeholder-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700 transition-all duration-200"
+              />
+            </form>
+
+            <form onSubmit={handleFilterByRegion}>
+              <select
+                name="filter-by-region"
+                id="filter-by-region"
+                className="w-52 py-3 px-4 outline-none shadow rounded text-gray-600 dark:text-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700"
+                value={regions.name}
+                onChange={(e) => filterByRegion(e.target.value)}
+              >
+                {regions.map((region, index) => (
+                  <option value={region.name} key={index}>
+                    {region.name}
+                  </option>
+                ))}
+              </select>
+            </form>
+          </div>
+          <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4">
+            {countries.map((country) => (
+              <Article key={country.name.common} {...country} />
+            ))}
+          </div>
         </section>
       )}
     </>
